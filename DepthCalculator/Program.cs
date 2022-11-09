1e466a6e102fcf960aa260652a1bd840481c4e25
Edgaras <Edgaras@SSI000304>
2022-11-09 09:07:50 +0200

    Add project files.

---
+++ DepthCalculator/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace DepthCalculator
+{
+    class Branch
+    {
+        public List<Branch> branches;
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test.
+            Branch branch = new();
+            branch.branches.Add(new());
+
+            Branch branch2 = new();
+            branch2.branches.Add(branch);
+            branch2.branches.Add(new());
+
+            Console.WriteLine(CalculateDepth(branch2)); // 3.
+        }
+
+        // Recursively finds the depth of a Branch structure.
+        static int CalculateDepth(Branch branch)
+        {
+            // Base case.
+            if (branch.branches.Count == 0)
+            {
+                return 1;
+            }
+
+            // Get the sub branch with the highest depth.
+            int highestDepth = CalculateDepth(branch.branches[0]);
+            for (int i = 1; i < branch.branches.Count; i++)
+            {
+                int depth = CalculateDepth(branch.branches[i]);
+                if (depth > highestDepth)
+                {
+                    highestDepth = depth;
+                }
+            }
+
+            return 1 + highestDepth;
+        }
+    }
+}
